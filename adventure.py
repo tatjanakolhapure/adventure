abd03fa04c61ec67ddf8e30faa8f13891e3a6076
Tatjana Kolhapure <tatjanagkolhapure@gmail.com>
2017-03-23 20:03:15 +0000

    Added description to locations and objects to pick up at each location

--- adventure.py
+++ adventure.py
@@ -9,16 +9,36 @@ directions = {
 
 position = (0, 0)
 
+pocket = []
+
 while True:
-    location = locations[position]
+    location = locations[position]['name']
     print 'you are at the %s' % location
+    description = locations[position]['description']
+    print description
+
+    items = locations[position]['objects']
+    print 'you can pick up here:'
+    for i in items:
+        print i
+
+    item = raw_input('what would you like to pick up?\n')
+    if item not in items:
+        print 'sorry, this item is not available'
+    elif item in pocket:
+        print 'you have this item in your pocket already'
+    else:
+        pocket.append(item)
+        print 'you have in your pocket:'
+        for i in pocket:
+            print i
 
     valid_directions = {}
     for k, v in directions.iteritems():
         possible_position = (position[0] + v[0], position[1] + v[1])
         possible_location = locations.get(possible_position)
         if possible_location:
-            print 'to the %s is a %s' % (k, possible_location)
+            print 'to the %s is a %s' % (k, possible_location['name'])
             valid_directions[k] = possible_position
 
     direction = raw_input('which direction do you want to go?\n')
--- data.py
+++ data.py
@@ -1,6 +1,31 @@
+house = {
+    'name':'house',
+    'description':'this is your home',
+    'objects':('book','music player','phone')
+}
+
+lake = {
+    'name':'lake',
+    'description':'it is peaceful and quiet place',
+    'objects':('stone','twig')
+}
+
+park = {
+    'name':'park',
+    'description':'you can have a walk here',
+    'objects':('flower','frisby')
+}
+
+market = {
+    'name':'market',
+    'description':'you can buy different things here',
+    'objects':('pizza','coke','shoes')
+}
+
+
 locations = {
-    (0, 0): 'house',
-    (0, 1): 'lake',
-    (1, 0): 'park',
-    (1, 1): 'market',
+    (0, 0): house,
+    (0, 1): lake,
+    (1, 0): park,
+    (1, 1): market
 }
